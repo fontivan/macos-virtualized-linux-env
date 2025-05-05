f32e69aa3700172bfc636901dbed2af04b0311da
Steven Skeard <stevenskeard@gmail.com>
2025-05-05 17:49:26 -0400

    Update build script to reliably work in paths with interesting directory names

--- podman/build.sh
+++ podman/build.sh
@@ -8,15 +8,17 @@ source "${SCRIPT_DIR}/common.sh"
 function build {
     local arch
     arch=$1
-    cd $SCRIPT_DIR
-    podman build \
-        --arch ${arch} \
-        --build-arg AMD64_AARCH64_ARCH="${arch}" \
-        --build-arg AMD64_ARM64_ARCH="$(echo ${arch} | sed 's/aarch64/arm64/')" \
-        --build-arg SUBSCRIPTION_MANAGER_ORG="${SUBSCRIPTION_MANAGER_ORG}" \
-        --build-arg SUBSCRIPTION_MANAGER_KEY="${SUBSCRIPTION_MANAGER_KEY}" \
-        -t "${TAG}-${arch}" \
-        .
+    (
+        cd "${SCRIPT_DIR}"
+        podman build \
+            --arch ${arch} \
+            --build-arg AMD64_AARCH64_ARCH="${arch}" \
+            --build-arg AMD64_ARM64_ARCH="$(echo ${arch} | sed 's/aarch64/arm64/')" \
+            --build-arg SUBSCRIPTION_MANAGER_ORG="${SUBSCRIPTION_MANAGER_ORG}" \
+            --build-arg SUBSCRIPTION_MANAGER_KEY="${SUBSCRIPTION_MANAGER_KEY}" \
+            -t "${TAG}-${arch}" \
+            .
+    )
 }
 
 function main {
